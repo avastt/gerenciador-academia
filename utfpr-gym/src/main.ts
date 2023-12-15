b9d54d13e20f63ca38cb70b85fe19d9010a6a42c
Igor Frey <igorfreyy97@gmail.com>
2023-12-15 18:47:15 -0300

    Uso de pipe para Data na listagem

--- utfpr-gym/src/main.ts
+++ utfpr-gym/src/main.ts
@@ -1,7 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { LOCALE_ID } from '@angular/core';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+.bootstrapModule(AppModule, {
+  providers: [{provide: LOCALE_ID, useValue: 'PT-BR'}],
+})
   .catch(err => console.error(err));
